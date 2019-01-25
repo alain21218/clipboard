7c0957e828bcdd3e72c1983a8af27b453212ccf6
xsac4438 <alain.soumagne@police.etat.lu>
2019-01-25 17:55:37 +0100

    merge

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 - Ajouter recherche - ok
-<<<<<<< HEAD
 - Ajouter suppression item - ok
 - Ajouter clear history - ok
 - Ajouter images - ok
@@ -7,9 +6,3 @@
 
 - animation - ok
 - confirmation - ok
-=======
-- Ajouter suppression item
-- Ajouter clear history - ok
-- Ajouter images - ok
-- bug edit premier element
->>>>>>> 9e10570ed6fab2448bb74fb6924b283637ca410c
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,18 +4,13 @@ import { clipboard, nativeImage } from 'electron';
 import { ElectronService } from '../../providers/electron.service';
 import { SortByPipe } from '../../pipes/sort-by.pipe';
 import { ClipItem } from '../../models/clip-item';
-<<<<<<< HEAD
 import { animations } from '../../shared/animations';
 import { Observable } from 'rxjs';
 import { ConfirmationService } from '../confirmation/confirmation.service';
-=======
-import { AppConfig } from '../../../environments/environment.prod';
->>>>>>> 9e10570ed6fab2448bb74fb6924b283637ca410c
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-<<<<<<< HEAD
   styleUrls: ['./home.component.scss'],
   animations
 })
@@ -23,11 +18,6 @@ export class HomeComponent implements OnInit {
   load = true;
   loadObs = new Observable();
 
-=======
-  styleUrls: ['./home.component.scss']
-})
-export class HomeComponent implements OnInit {
->>>>>>> 9e10570ed6fab2448bb74fb6924b283637ca410c
   repeat: NodeJS.Timer;
   clipItems: ClipItem[];
   searchKey: string;
@@ -41,7 +31,6 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private snackbar: MatSnackBar,
-<<<<<<< HEAD
     private electronService: ElectronService,
     private confirm: ConfirmationService
   ) { }
@@ -50,19 +39,11 @@ export class HomeComponent implements OnInit {
     this.startSpinner(1000);
     this.retrieve();
 
-=======
-    private electronService: ElectronService
-  ) { }
-
-  ngOnInit() {
-    this.retrieve();
->>>>>>> 9e10570ed6fab2448bb74fb6924b283637ca410c
     this.repeat = setInterval(() => {
       this.imageTreatment();
       this.textTreatment();
       this.getCurrentClipboard();
     }, 250);
-<<<<<<< HEAD
 
   }
 
@@ -79,8 +60,6 @@ export class HomeComponent implements OnInit {
         resolve();
       }, time);
     });
-=======
->>>>>>> 9e10570ed6fab2448bb74fb6924b283637ca410c
   }
 
   retrieve() {
@@ -99,19 +78,11 @@ export class HomeComponent implements OnInit {
       }
     } else { this.text = null; }
   }
-<<<<<<< HEAD
 
   save() {
     localStorage.setItem('cb', JSON.stringify(this.clipItems))
   }
 
-=======
-  
-  save() {
-    localStorage.setItem('cb', JSON.stringify(this.clipItems))
-  }
-  
->>>>>>> 9e10570ed6fab2448bb74fb6924b283637ca410c
   imageTreatment() {
     const imageNative = clipboard.readImage();
     this.image = imageNative.toDataURL();
@@ -127,15 +98,9 @@ export class HomeComponent implements OnInit {
   }
 
   getCurrentClipboard() {
-<<<<<<< HEAD
     if (this.text) {
       this.currentClipboard = this.clipItems.find(item => item.text === this.text);
     } else if (this.image) {
-=======
-    if(this.text) {
-      this.currentClipboard = this.clipItems.find(item => item.text === this.text);
-    } else if(this.image) {
->>>>>>> 9e10570ed6fab2448bb74fb6924b283637ca410c
       this.currentClipboard = this.clipItems.find(item => item.image === this.image);
     } else {
       this.currentClipboard = null;
@@ -147,19 +112,11 @@ export class HomeComponent implements OnInit {
   }
 
   copy(clipitem: ClipItem) {
-<<<<<<< HEAD
     if (clipitem.text) {
       this.copyText(clipitem.text);
     }
 
     if (clipitem.image) {
-=======
-    if(clipitem.text) {
-      this.copyText(clipitem.text);
-    }
-
-    if(clipitem.image) {
->>>>>>> 9e10570ed6fab2448bb74fb6924b283637ca410c
       this.copyImage(clipitem.image);
     }
   }
@@ -182,7 +139,6 @@ export class HomeComponent implements OnInit {
   }
 
   clear() {
-<<<<<<< HEAD
     this.confirm.ask('Supprimer les éléments marqués (☆) ?', () => {
       this.confirm.ask('Tout supprimer ?', () => {
         localStorage.clear();
@@ -193,13 +149,6 @@ export class HomeComponent implements OnInit {
       this.clipItems = this.clipItems.filter(item => item.favorite);
       this.save();
     });
-=======
-    if (confirm('Supprimer tout ?')) {
-      localStorage.clear();
-      clipboard.clear();
-      this.clipItems.length = 0;
-    }
->>>>>>> 9e10570ed6fab2448bb74fb6924b283637ca410c
   }
 
   search($event) {
@@ -225,10 +174,6 @@ export class HomeComponent implements OnInit {
   favorite(item: ClipItem) {
     item.favorite = !item.favorite;
     this.save();
-<<<<<<< HEAD
-=======
-    this.retrieve(); // update sort
->>>>>>> 9e10570ed6fab2448bb74fb6924b283637ca410c
   }
 
   delete(clipitem: ClipItem) {
